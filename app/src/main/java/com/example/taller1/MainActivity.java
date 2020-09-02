ebf41ae0637f3876b46f0fa446940403acaaa12a
Santiago Caro <santiagocaroduque@gmail.com>
2020-09-02 10:00:27 -0500

    Fix Spinner entries

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.taller1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayList<Integer> opciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+
+        fillArray();
+
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, opciones);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+
     }
+
+    private void fillArray() {
+        opciones = new ArrayList<>();
+        for (int i=0;i<16;i++){
+            opciones.add(i);
+        }
+
+    }
+
 }
