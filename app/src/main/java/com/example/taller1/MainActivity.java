1bac096c2c8a604058872ba2cdcf154ac91c721b
Nicolas Lopez <nlopez97f@gmail.com>
2020-09-02 10:38:12 -0500

    Try-NotOpen-StillNo

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -1,26 +1,18 @@
 package com.example.taller1;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button fiboBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Info","Aca");
 
-        fiboBtn = findViewById(R.id.FibonacciBtn);
-
-        fiboBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(), FiboActivity.class));
-            }
-        });
     }
 }
