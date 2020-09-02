56b841589823d2555d28e1b2232fb86ca3f35a0c
Santiago Caro <santiagocaroduque@gmail.com>
2020-09-02 10:13:13 -0500

    Added FactorialActivity and intent from main

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.taller1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -12,19 +15,31 @@ public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
     ArrayList<Integer> opciones;
+    Button factorial,fibonacci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Inflate
         spinner = findViewById(R.id.spinner);
+        factorial = findViewById(R.id.FactorialBtn);
+        fibonacci = findViewById(R.id.FibonacciBtn);
 
         fillArray();
 
         ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, opciones);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
+
+        factorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),FactorialActivity.class);
+                intent.putExtra("factorial",spinner.getSelectedItem().toString());
+                startActivity(intent);
+            }
+        });
 
 
     }
